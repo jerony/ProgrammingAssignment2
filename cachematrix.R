f2214f66e6d30e1f934d27ee245483c83b82a430
jerony <jryoungwdl@gmail.com>
2017-04-09 19:53:41 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MakeCacheMatrix and cacheSolve are a pair of fuctions that interact in a way 
+## that allows you to store and recall the inverse of a matrix, (and reset this
+## stored inverse if the underlying matrix changes).
 
-## Write a short comment describing this function
+## MakeCacheMatrix creates an object that stores the current value of the inverse
 
-makeCacheMatrix <- function(x = matrix()) {
+MakeCacheMatrix <- function(x = matrix()) {
+		t <- NULL
+            retrm <- function() {print(x)}
+            setinv <- function(inverse){t <<- inverse}
+            retrinv <- function() {t}
+		structure(list(retrm=retrm,setinv=setinv,retrinv=retrinv,filler=NULL),dim=c(2,2))
+                          }
 
-}
+## cacheSolve retrieves the inverse stored in the body of MakeCacheMatrix if
+## it is not a null value; if it is a null value, this function calculates the 
+## inverse of the matrix, then stores it in the body of MakeCacheMatrix.
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(m,...) {
+		n <- m$retrinv()
+	      if(!is.null(n)) {
+			message("retrieve stored inverse")
+                  return(n) }
+                  else {
+				  q <- m$retrm()
+                          w <- solve(q,...)
+                          z <- m$setinv(w)
+                          w }
+                 }
